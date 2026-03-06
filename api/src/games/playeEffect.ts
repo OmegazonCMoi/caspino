f6ef8945ba1e087b64f9e678ff988f6e2aa484bd
aina-carpentier <aina.carpentier@etu.univ-smb.fr>
2026-03-06 12:15:56 +0100

    fix: redirect to second raspberry

--- api/src/games/playeEffect.ts
+++ api/src/games/playeEffect.ts
@@ -1,27 +1,23 @@
 export const playeEffect = (gains: number, bet: number): void => {
-
-    const factor = gains / bet
-    if (factor < 1) {
-        fetch("http://localhost:5701/siren/small", {
-            method: "POST"
-        }).catch(() => {});
-    }
-    else if (factor < 5) {
-        fetch("http://localhost:5701/siren/medium", {
-            method: "POST"
-        }).catch(() => {});
-    }
-    else if (factor < 10) {
-        fetch("http://localhost:5701/plug/blink", {
-            method: "POST"
-        }).catch(() => {})
-        fetch("http://localhost:5701/siren/big", {
-            method: "POST"
-        }).catch(() => {})
-    }
-    else {
-        fetch("http://localhost:5701/casino/jackpot", {
-            method: "POST"
-        }).catch(() => {})
-    }
+  const factor = gains / bet
+  if (factor < 1) {
+    fetch("http://10.109.150.245:5701/siren/small", {
+      method: "POST",
+    }).catch(() => {})
+  } else if (factor < 5) {
+    fetch("http://10.109.150.245:5701/siren/medium", {
+      method: "POST",
+    }).catch(() => {})
+  } else if (factor < 10) {
+    fetch("http://10.109.150.245:5701/plug/blink", {
+      method: "POST",
+    }).catch(() => {})
+    fetch("http://10.109.150.245:5701/siren/big", {
+      method: "POST",
+    }).catch(() => {})
+  } else {
+    fetch("http://10.109.150.245:5701/casino/jackpot", {
+      method: "POST",
+    }).catch(() => {})
+  }
 }
